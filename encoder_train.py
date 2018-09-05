840d0f7d4e2c77cbef7eae50efe405be434297d1
paisuygoda <paisuygoda3@gmail.com>
2018-09-05 18:41:13 +0900

    bugfix

--- encoder_train.py
+++ encoder_train.py
@@ -181,7 +181,7 @@ def extract_feature(encoder, dataloader, max_length, feat_dim):
             encoder.lstm.flatten_parameters()
             encoder_output, encoder_hidden = encoder(torch.autograd.Variable(original_variable.data.narrow(1, i, 1)).cuda(),
                                                      encoder_hidden)
-        features = encoder_hidden[0].data.cpu().view(batch_size, feat_dim).numpy()
+        features = encoder_hidden.data.cpu().view(batch_size, feat_dim).numpy()
         user_id = user_id.data.cpu().view(batch_size).numpy()
         firstday = firstday.data.cpu().view(batch_size.numpy())
         for user, day, feature in zip(user_id, firstday, features):
